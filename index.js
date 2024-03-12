89eb5a5d2f293c88d56dc2c51fb241b4fe1b18b9
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2024-03-12 14:49:25 +1300

    init

---
+++ index.js
@@ -0,0 +1,75 @@
+// Import necessary packages
+let mysql = require('mysql');
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+
+// Initialize app
+const app = express();
+websiteInit();
+
+require('dotenv').config();
+
+// Create connection with SQL database
+let con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.MYSQL_PASSWORD,
+    database: 'mydatabase'
+});
+
+// Connect to SQL database and log "Connected!" on success as well as the result
+con.connect(function (err, result) {
+    if (err) throw err;
+    console.log('Connected!', result);
+
+    con.query('SELECT * FROM customers', function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+    });
+
+    con.end(function (err) {
+        if (err) {
+            return console.log('error:' + err.message);
+        }
+        console.log('Database connection closed.');
+    });
+});
+
+const dashboardRoutes = require('./routes/x');
+
+async function websiteInit() {
+    // Set 'views' directory for any views 
+    // being rendered res.render()
+    app.set('views', path.join(__dirname, 'views'));
+
+    // Set template html thingy to EJS
+    app.set('view engine', 'ejs');
+
+    // Set /public to being the JS and CSS source
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    // parse application/x-www-form-urlencoded
+    app.use(express.urlencoded({ extended: false }));
+
+    // parse application/json
+    app.use(express.json());
+
+    // Setting up session
+    app.use(session({
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: false,
+        cookie: { secure: 'auto' }
+    }));
+
+    // Routes to be used
+    app.use(routes);
+
+    // Listen on port 3000
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000, http://localhost:3000');
+    });
+}
+
+module.exports = dashboardInit;
