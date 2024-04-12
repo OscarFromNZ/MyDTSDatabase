f9eb00ebea867fb7151565d7090a474f68ff2a24
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2024-04-12 12:12:44 +1200

    stuff

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    res.redirect('login');
+    res.render('home', { message: '' });
+    //res.redirect('login');
 });
 
 module.exports = router;
--- routes/switchboardRoutes.js
+++ routes/switchboardRoutes.js
@@ -13,4 +13,24 @@ router.get('/switchboard', ensureAuthenticated, async (req, res) => {
     res.render('switchboard', { message: message });
 });
 
+router.get('/switchboard/form/:formName', ensureAuthenticated, async(req, res) => {
+    let formName = req.params.formName;
+
+    // this data is layout stuff only, to determine the html
+    let formLayoutData = req.app.formLayoutData.get(formName);
+
+    res.render(formName, { message: '', formLayoutData });
+});
+
+router.get('/switchboard/report/:reportName', ensureAuthenticated, async(req, res) => {
+    // get data for report (layout as well as actual stuff)
+    let reportName = req.params.reportName;
+    let reportLayoutData = req.app.reportDataGetter.get(reportName);
+
+    res.render(reportName, { message: '', reportData });
+});
+
 module.exports = router;
+
+
+// wait, I think we only need the query data and we cna do the if statements in the html
