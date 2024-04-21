6e7339d3460fa5d09a12d0a012b23782d150f59c
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2024-04-21 19:05:48 +1200

    some backendy stuff

--- routes/backendRoutes.js
+++ routes/backendRoutes.js
@@ -1,9 +1,39 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/backend', async (req, res) => {
+router.get('/structure', async (req, res) => {
     let databaseStructure = await req.app.database.getDatabaseStructure();
-    res.render('backend', { databaseStructure, message: '' });
+    res.render('structure', { databaseStructure, message: '' });
+});
+
+router.get('/database', async (req, res) => {
+    let tables = await req.app.database.getDatabaseStructure();
+    res.render('database', { message: '', tables });
+});
+
+router.get('/database/:tableName', async (req, res) => {
+    let tableName = req.params.tableName;
+
+    // wait maybe don't even need to do this
+    let tableLayoutDatas = await req.app.database.getDatabaseStructure();
+    let tableLayoutDataIndex = 0;
+
+    // I'm sure there's a 1 liner for this but who cares, I supose you could use .filter
+    for (let i = 0; i < tableLayoutDatas.length; i++) {
+        if (tableName = tableLayoutDatas[i].name) {
+            tableLayoutDataIndex = i;
+        }
+    }
+    // writing that for loop gave me a massive deja vu
+
+    let selectAllQuery = `SELECT * from ${tableName}`;
+
+    req.app.database.executeQuery(selectAllQuery, function(result) {
+        console.log(result);
+        res.redirect(`/switchboard/form/${formName}?message=Successfully deleted customer with ID ${req.body.CustomerID}`);
+    });
+
+    res.render(`database/${tableName}`, { message: '' });
 });
 
 module.exports = router;
